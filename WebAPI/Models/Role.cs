a61d6650376eb924c55bfb3119de85d8c6556714
Luis Fernando Ramirez <luisf350@gmail.com>
2019-05-20 16:52:41 -0500

    Agregar archivos de proyecto.

---
+++ WebAPI/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Role
+    {
+        public const string Admin = "Admin";
+
+        public const string User = "User";
+    }
+}
---
+++ WebAPI/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+
+        IEnumerable<User> GetAll();
+
+        User GetById(int id);
+    }
+}
